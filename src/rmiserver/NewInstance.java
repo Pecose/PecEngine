1ea2dccae29a72232050c1608cadc39438581ea7
Pecose <pecose.billos@gmail.com>
2021-08-18 04:18:45 +0200

    Update NewInstance.java

--- src/rmiserver/NewInstance.java
+++ src/rmiserver/NewInstance.java
@@ -5,12 +5,13 @@ import java.io.Serializable;
 public class NewInstance implements Serializable{
 
 	private Serializable target;
+	private String name;
 	
-	public Serializable getTarget() {
-		return this.target;
-	}
+	public Serializable getTarget() { return this.target; }
+	public String getName() { return this.name; }
 
-	public NewInstance(Serializable target) {
+	public NewInstance(Serializable target, String name) {
 		this.target = target;
+		this.name = name;
 	}
 }
